1a7e7509003f2c4feaf03a7e18acc7cd1fc1e3c8
KhaLe <minhkhanb@gmail.com>
2017-06-06 16:24:10 +0700

    init

--- js/start.js
+++ js/start.js
@@ -18,25 +18,40 @@ graph.popover = function () {
         getTokenUI = document.querySelector(".getTokenUI"),
         offsetTokenUI = getTokenUIElem.getBoundingClientRect();
 
+    var methodElem = document.querySelector(".methodElem"),
+        methodUI = document.querySelector(".methodUI"),
+        offsetMethodUI = methodElem.getBoundingClientRect();
+
     getTokenUIElem.addEventListener("click", function (evt) {
        getTokenUI.style.top = offsetTokenUI.top + offsetTokenUI.height + 'px';
        getTokenUI.style.left = offsetTokenUI.left + 'px';
     });
+
+    methodElem.addEventListener("click", function (evt) {
+       methodUI.style.top = offsetMethodUI.top + offsetMethodUI.height + 'px';
+       methodUI.style.left = offsetMethodUI.left + 'px';
+    });
+
     document.addEventListener("click", function (evt) {
         var el = evt.target,
             closest,
             _target;
+        console.log(evt.target);
         if(el.tagName.toLowerCase() === 'a') {
-            _target = helpers.hasClass(el.classList, 'getTokenUIElem');
+            _target = helpers.hasClass(el.classList, 'getTokenUIElem') || helpers.hasClass(el.classList, 'methodElem');
         }
         else {
             closest = el.parentNode.parentNode;
-            _target = helpers.hasClass(closest.classList, 'getTokenUIElem');
+            _target = helpers.hasClass(closest.classList, 'getTokenUIElem') || helpers.hasClass(el.parentNode.classList, 'methodElem');
         }
 
         if(_target !== "getTokenUIElem") {
             getTokenUI.style.top = "";
             getTokenUI.style.left = "";
+        }
+        if(_target !== "methodElem") {
+            methodUI.style.top = "";
+            methodUI.style.left = "";
         }
     });
 }
